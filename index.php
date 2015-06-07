15ddb286debd2eda95bbd9ecd1ad17cf173d3d50
Hussain Khalil <hussainpilot05@gmail.com>
2015-06-07 13:30:04 -0700

    Added superfast

--- index.php
+++ index.php
@@ -1,62 +1,65 @@
 <?php
-include "scripts/php/simple_html_dom.php";
-function findTeachers($mobile) {
-	$html = file_get_html('http://cv.sduhsd.net/staff/');
-	$rows = $html->find("#sites-canvas-main-content table tbody tr td div center table tbody tr");
-	$count = count($rows);
-	$i = 1;
-	$array = array();
-	foreach($rows as $element) {
-		$returnArray = array();
-		if($i < $count && $i !== 1) {
-			foreach($element->find("td") as $td) {
-				if(strstr($td->plaintext,"Email")) {
-					$email = $td->find("a");
-					if(count($email) > 0) {
-						$email = $email[0]->href;
-						$returnArray[] = $email;
-					} else {
-						$returnArray[] = "";
-					}
-				} elseif(strstr($td->plaintext,"Website")) {
-					$url = $td->find("a");
-					if(count($url) > 0) {
-						$url = $url[0]->href;
-						$returnArray[] = $url;
-					} else {
-						$returnArray[] = "";
-					}
-				} else {
-					$trimmed = $td->plaintext;
-					$returnArray[] = $trimmed;
-				}
-			}
-		}
-		$i++;
-		$array[] = $returnArray;
-	};
-	unset($array[0]);
-	unset($array[count($array)]);
-	$newArray = array();
-	foreach($array as $row) {
-		$newArrayRow = array();
-		$newArrayRow["Name"] = $row[0];
-		$newArrayRow["Subject"] = $row[1];
-		$newArrayRow["Email"] = $row[2];
-		$newArrayRow["SoftEmail"] = str_replace("mailto:","",$row[2]);
-		$newArrayRow["Website"] = $row[3];
-		if(!$mobile) {
-			$newArrayRow["SoftUrl"] = str_replace(array("https://","http://"),"",$row[3]);
-		} else {
-			$newArrayRow["SoftUrl"] = "Website";
-		}
-		$newArray[$row[0]] = $newArrayRow;
-	}
-	$array = $newArray;
-	return str_replace("\\u00a0","",json_encode($array));
-}
 if(isset($_GET['ajax'])) {
-	echo findTeachers(isset($_GET['mobile']));
+	$yesterday = time() - 86400;
+	if(!file_exists("static/cache/list") || filemtime("static/cache/list") < $yesterday || isset($_GET['gen'])) {
+		include "scripts/php/simple_html_dom.php";
+		function findTeachers() {
+			$html = file_get_html('http://cv.sduhsd.net/staff/');
+			$rows = $html->find("#sites-canvas-main-content table tbody tr td div center table tbody tr");
+			$count = count($rows);
+			$i = 1;
+			$array = array();
+			foreach($rows as $element) {
+				$returnArray = array();
+				if($i < $count && $i !== 1) {
+					foreach($element->find("td") as $td) {
+						if(strstr($td->plaintext,"Email")) {
+							$email = $td->find("a");
+							if(count($email) > 0) {
+								$email = $email[0]->href;
+								$returnArray[] = $email;
+							} else {
+								$returnArray[] = "";
+							}
+						} elseif(strstr($td->plaintext,"Website")) {
+							$url = $td->find("a");
+							if(count($url) > 0) {
+								$url = $url[0]->href;
+								$returnArray[] = $url;
+							} else {
+								$returnArray[] = "";
+							}
+						} else {
+							$trimmed = $td->plaintext;
+							$returnArray[] = $trimmed;
+						}
+					}
+				}
+				$i++;
+				$array[] = $returnArray;
+			};
+			unset($array[0]);
+			unset($array[count($array)]);
+			$newArray = array();
+			foreach($array as $row) {
+				$newArrayRow = array();
+				$newArrayRow["Name"] = $row[0];
+				$newArrayRow["Subject"] = $row[1];
+				$newArrayRow["Email"] = $row[2];
+				$newArrayRow["SoftEmail"] = str_replace("mailto:","",$row[2]);
+				$newArrayRow["Website"] = $row[3];
+				$newArrayRow["SoftUrl"] = str_replace(array("https://","http://"),"",$row[3]);
+				$newArray[$row[0]] = $newArrayRow;
+			}
+			$array = $newArray;
+			return str_replace("\\u00a0","",json_encode($array));
+		}
+		$data = findTeachers();
+		echo $data;
+		file_put_contents("static/cache/list",$data);
+	} else {
+		echo file_get_contents("static/cache/list");
+	}
 	exit();
 }
 ?>
@@ -176,38 +179,45 @@ if(isset($_GET['ajax'])) {
 			}
 			function mobile() {
 				var check = false;
-				(function(a,b){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4)))check = true})(navigator.userAgent||navigator.vendor||window.opera);
+				(function(a,b){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4)))check = true;})(navigator.userAgent||navigator.vendor||window.opera);
 				return check;
 			}
+			var ismobile;
+			if(mobile()) {
+				$("#search").attr("placeholder","Name or subject...");
+				ismobile = true;
+			} else {
+				$("#search").attr("placeholder","Search for a teacher by name or subject...");
+				ismobile = false;
+			}
 			function draw(obj) {
-				$("#table").html($("#table").html()+"<tr><td>"+obj.Name+"</td><td>"+obj.Subject+"</td><td><a href='"+obj.Email+"'>"+obj.SoftEmail+"</a></td><td class='site'><a href='"+obj.Website+"'>"+obj.SoftUrl+"</a></td></tr>");
+				var softurl;
+				if(ismobile) {
+					softurl = "Website";
+				} else {
+					softurl = obj.SoftUrl;
+				}
+				$("#table").html($("#table").html()+"<tr><td>"+obj.Name+"</td><td>"+obj.Subject+"</td><td><a href='"+obj.Email+"'>"+obj.SoftEmail+"</a></td><td class='site'><a href='"+obj.Website+"'>"+softurl+"</a></td></tr>");
 			}
 			function clear() {
 				$("#table").html("<tr><th>Teacher</th><th>Subject</th><th>Email</th><th>Website</th></tr>");
 			}
 			$("#search").on('keyup',function(event) {
 				var keycode = (event.keyCode ? event.keyCode : event.which);
-				if(keycode == '13'){
+				if(keycode == '13') {
 					findFirst();
 				}
 			});
 			var teachers;
 			$(document).ready(function() {
-				var ajaxURL = "/?ajax";
-				if(mobile()) {
-					$("#search").attr("placeholder","Name or subject...");
-					ajaxURL += "&mobile";
-				} else {
-					$("#search").attr("placeholder","Search for a teacher by name or subject...");
-				}
 				var ajax = function(attempt) {
-					$.ajax(ajaxURL, {
+					$.ajax("/?ajax", {
 						beforeSend:function() {
 							$("#search").css("backgroundImage","url(/static/ui/loader.gif)");
 						},
 						error:function() {
 							if(attempt < 3) {
-								ajax(++attempt)
+								ajax(++attempt);
 							} else {
 								$("#search").val("An error occurred! Please try again.");
 								$("#search").select();
@@ -226,7 +236,7 @@ if(isset($_GET['ajax'])) {
 						},
 						timeout:5000
 					});
-				}
+				};
 				ajax(0);
 				var last = "";
 				var falsesearch = false;
